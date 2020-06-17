4979a5c19da979501da97c5bcc0139459684fb0f
alfixed <alfixed@gmail.com>
2020-06-16 21:31:53 -0500

    add redux, action, reducers, connect, persisting

--- todos/TodoListItem.js
+++ todos/TodoListItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ({todo}) => (
+const TodoListItem = ({ todo, onRemovePressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="button-container">
             <button className="complited-button">Mark As Complited</button>
-            <button className="remove-button">Remove</button>
+            <button
+                onClick={() => onRemovePressed(todo.text)}
+                className="remove-button"
+            >
+                Remove
+            </button>
         </div>
     </div>
 );

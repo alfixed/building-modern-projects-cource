81e3e7a6a653dac79c2261ffd01196896d21d850
alfixed <alfixed@gmail.com>
2020-06-17 13:05:12 -0500

    add action for buttom Mark As Completed

--- todos/TodoList.js
+++ todos/TodoList.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
-import { removeTodo } from './action';
+import { removeTodo, markTodoAsCompleted } from './action';
 import './TodoList.css';
 
-const TodoList = ({ todos=[], onRemovePressed }) => (
+const TodoList = ({ todos=[], onRemovePressed, onCompletedPressed }) => (
     <div className="list-wrapper">
         <NewTodoForm />
-        { todos.map(todo => <TodoListItem todo={ todo } onRemovePressed={ onRemovePressed } />) }
+        { todos.map(todo =>
+            <TodoListItem
+                todo={ todo }
+                onRemovePressed={ onRemovePressed }
+                onCompletedPressed={ onCompletedPressed }    
+            />) }
     </div>
 );
 
@@ -17,7 +22,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    onRemovePressed: text => dispatch(removeTodo(text))
+    onRemovePressed: text => dispatch(removeTodo(text)),
+    onCompletedPressed: text => dispatch(markTodoAsCompleted(text))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- todos/TodoListItem.js
+++ todos/TodoListItem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ({ todo, onRemovePressed }) => (
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="button-container">
-            <button className="complited-button">Mark As Complited</button>
+            { todo.isCompleted 
+                ? null
+                : <button
+                    onClick={() => onCompletedPressed(todo.text)}
+                    className="complited-button"
+                >
+                    Mark As Complited
+                </button>
+            }
             <button
                 onClick={() => onRemovePressed(todo.text)}
                 className="remove-button"
